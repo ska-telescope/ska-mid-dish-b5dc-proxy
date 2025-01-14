fe894625bfdff9456fd345e8ef12ba3a9b27c7d6
Mpumelelo Nzama <mnzama@sarao.ac.za>
2025-01-14 11:27:17 +0200

    KAR-1314 Init component manager class and parent class cm components states

--- src/ska_mid_dish_b5dc_proxy/B5dc_cm.py
+++ src/ska_mid_dish_b5dc_proxy/B5dc_cm.py
@@ -17,6 +17,9 @@ class B5dcDeviceComponentManager(TaskExecutorComponentManager):
     
     def __init__(
         self: "B5dcDeviceComponentManager",
+        b5dc_server_ip: str,
+        b5dc_server_port: int,
+        b5dc_sensor_update_period: int,
         logger: logging.Logger,
         *args: Any, 
         **kwargs: Any,     
@@ -25,6 +28,22 @@ class B5dcDeviceComponentManager(TaskExecutorComponentManager):
 
         # TODO: Make configurable
         self.logger.setLevel(logging.DEBUG)
+        super().__init__(
+            logger,
+            *args,
+            spi_rfcm_frequency=0,
+            spi_rfcm_pll_lock=B5dcPllState.NOT_LOCKED,
+            spi_rfcm_h_attenuation=0,
+            spi_rfcm_v_attenuation=0,
+            spi_rfcm_photo_diode_ain0=0,
+            spi_rfcm_rf_in_h_ain1=0,
+            spi_rfcm_rf_in_v_ain2=0,
+            spi_rfcm_if_out_h_ain3=0,
+            spi_rfcm_if_out_v_ain4=0,
+            spi_rfcm_rf_temp_ain5=0,
+            spi_rfcm_psu_pcb_temp_ain7=0,
+            **kwargs,
+        )
 
     def _update_component_state(self, *args, **kwargs):
         # TODO: For every component update emit a change event <- Done in component_state_callback in ds
