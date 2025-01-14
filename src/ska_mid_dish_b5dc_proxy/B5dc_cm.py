d9350f6dd2519169beeb8d0401f55e088add9635
Mpumelelo Nzama <mnzama@sarao.ac.za>
2025-01-14 11:29:55 +0200

    KAR-1314 Added async method to estalish and maintain server connection for cm lifecycle

--- src/ska_mid_dish_b5dc_proxy/B5dc_cm.py
+++ src/ska_mid_dish_b5dc_proxy/B5dc_cm.py
@@ -28,6 +28,22 @@ class B5dcDeviceComponentManager(TaskExecutorComponentManager):
 
         # TODO: Make configurable
         self.logger.setLevel(logging.DEBUG)
+        
+        self.server_addr = (b5dc_server_ip, b5dc_server_port)
+
+        self.loop = asyncio.new_event_loop()
+        # self.on_con_lost = self.loop.create_future() <- This shouldnt be global because it completes and could prevent reconnections
+        
+        # Transport and protocol objects to be assigned on creation on datagram endpoint
+        self.transport = None
+        self.protocol = None
+        
+        # Start the event loop in a separate thread 
+        self.loop_thread = threading.Thread(target=self.start_event_loop, daemon=True) 
+        self.loop_thread.start()
+
+        # Establish server connection and keep event loop running in thread
+        asyncio.run_coroutine_threadsafe(self._establish_server_connection(), self.loop)
         super().__init__(
             logger,
             *args,
@@ -44,6 +60,36 @@ class B5dcDeviceComponentManager(TaskExecutorComponentManager):
             spi_rfcm_psu_pcb_temp_ain7=0,
             **kwargs,
         )
+    def start_event_loop(self): 
+        asyncio.set_event_loop(self.loop) 
+        self.loop.run_forever()
+
+    # Method that maintains connection to the server
+    async def _establish_server_connection(self) -> None:
+        while True:
+            self.server_connection_lost = self.loop.create_future()
+
+            self.transport, self.protocol = await self.loop.create_datagram_endpoint(
+                lambda: B5dcProtocol(self.server_conection_lost, 
+                    self.logger, 
+                    self.server_addr),
+                local_addr=("0.0.0.0", 0),
+                remote_addr=self.server_addr,
+            )
+            
+            self.logger.info("B5DC server connection created")
+            try:
+                await self.server_connection_lost
+                self.logger.warning("Connection to the underlying B5DC server lost. Attempting to reestablish one")
+            finally:
+                # Clean up transport and protocol and recreate transport
+                if self.transport:
+                    self.transport.close()
+                self.transport = None
+                self.protocol = None
+                await asyncio.sleep(5)
+
+
 
     def _update_component_state(self, *args, **kwargs):
         # TODO: For every component update emit a change event <- Done in component_state_callback in ds
