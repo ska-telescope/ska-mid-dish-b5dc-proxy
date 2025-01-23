59b1342e403bb5150f23a9bd6550c725c187546f
Mpumelelo Nzama <mnzama@sarao.ac.za>
2025-01-23 09:56:03 +0200

    KAR-1314 Added doc strings for b5dc proxy commands

--- src/ska_mid_dish_b5dc_proxy/b5dc_proxy.py
+++ src/ska_mid_dish_b5dc_proxy/b5dc_proxy.py
@@ -250,6 +250,10 @@ class B5dcProxy(SKAController):
     @command(
         dtype_in=int,
         dtype_out="DevVarLongStringArray",
+        doc_in="""Set the vertical polarization attenuation on the band 5 down converter.
+
+        :param attenuation_db: value to set in dB
+        """,
     )
     def SetVPolAttenuation(self: "B5dcProxy", attenuation_db: int) -> DevVarLongStringArrayType:
         """Set the vertical polarization attenuation on the band 5 down converter.
@@ -260,9 +264,16 @@ class B5dcProxy(SKAController):
         result_code, unique_id = handler(attenuation_db, "spi_rfcm_v_attenuation")
         return [result_code], [unique_id]
 
+    # NOTE: The cmd input cannot be enforced to take the ENUM, due to
+    # the enum value definitions (eg: F_11_1_GHZ=1,...). Tango only allows
+    # enums of value (0,1,2,3,4,...) as cmd input types
     @command(
         dtype_in=int,
         dtype_out="DevVarLongStringArray",
+        doc_in="""Set the frequency on the band 5 down converter.
+
+        :param frequency:B5dcFrequency  frequency to set
+        """,
     )
     def SetFrequency(self: "B5dcProxy", frequency: B5dcFrequency) -> DevVarLongStringArrayType:
         """Set the frequency on the band 5 down converter.
