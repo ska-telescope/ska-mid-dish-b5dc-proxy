56b8a017855c0396e5fcbc4d512af1e4921c455e
Mpumelelo Nzama <mnzama@sarao.ac.za>
2025-01-31 09:54:01 +0200

    KAR-1314 Modified B5dc unit tests to make use of b5dc_cm fixture in conftest

--- tests/unit/component_manager/test_b5dc_commands.py
+++ tests/unit/component_manager/test_b5dc_commands.py
@@ -1,14 +1,12 @@
 # pylint: disable=protected-access
 """Test component manager handling of unhappy path on SetFrequency cmd call."""
 import time
-from unittest.mock import AsyncMock, Mock, patch
+from unittest.mock import AsyncMock
 
 import pytest
 from ska_control_model import TaskStatus
 from ska_mid_dish_dcp_lib.device.b5dc_device import B5dcDeviceAttenuationException
 from ska_mid_dish_dcp_lib.device.b5dc_device_mappings import B5dcFrequency
-
-from ska_mid_dish_b5dc_proxy.b5dc_cm import B5dcDeviceComponentManager
 
 ATTENUATION_DB_IN_RANGE = 31
 ATTENUATION_DB_OUTSIDE_RANGE = -1
@@ -16,25 +14,9 @@ ATTENUATION_DB_OUTSIDE_RANGE = -1
 
 @pytest.mark.unit
 @pytest.mark.forked
-def test_b5dc_set_frequency_rejects_invalid_freq() -> None:
+def test_b5dc_set_frequency_rejects_invalid_freq(b5dc_cm_setup) -> None:
     """Verify SetFrequency returns TaskStatus.REJECTED for invalid freq. arg."""
-    update_period = 2
-    with patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcInterface", AsyncMock()), patch(
-        "ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcPropertyParser", Mock()
-    ), patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcDeviceConfigureFrequency", Mock()), patch(
-        "ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcProtocol", Mock()
-    ):
-        b5dc_cm = B5dcDeviceComponentManager("127.0.0.1", 10001, update_period, Mock())
-        b5dc_cm.start_communicating()
-
-    max_try = 5
-    for iterations in range(max_try):
-        if not b5dc_cm.is_connection_established():
-            time.sleep(1)
-        else:
-            break
-
-    assert iterations < max_try - 1, "Connection not established"
+    b5dc_cm, _ = b5dc_cm_setup
 
     invalid_frequency_enum_value = 5
 
@@ -61,25 +43,10 @@ def test_b5dc_set_frequency_rejects_invalid_freq() -> None:
     ],
 )
 def test_b5dc_set_frequency_method_with_valid_input(
-    frequency_to_set: B5dcFrequency, callbacks: dict
+    frequency_to_set: B5dcFrequency, callbacks: dict, b5dc_cm_setup
 ) -> None:
     """Verify SetFrequency returns expected LRC updates on supplying valid input."""
-    update_period = 2
-    with patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcInterface", AsyncMock()), patch(
-        "ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcPropertyParser", Mock()
-    ), patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcProtocol", Mock()):
-        b5dc_cm = B5dcDeviceComponentManager("127.0.0.1", 10001, update_period, Mock())
-        b5dc_cm.start_communicating()
-
-    max_try = 5
-    for iterations in range(max_try):
-        if not b5dc_cm.is_connection_established():
-            time.sleep(1)
-        else:
-            break
-
-    assert iterations < max_try - 1, "Connection not established"
-
+    b5dc_cm, _ = b5dc_cm_setup
     b5dc_cm._b5dc_device_freq_conf = AsyncMock()
 
     # Set the frequency and verify that expected lrc updates are published
@@ -120,24 +87,11 @@ def test_b5dc_set_frequency_method_with_valid_input(
         "spi_rfcm_v_attenuation",
     ],
 )
-def test_b5dc_set_attenuation_with_valid_input(attenuation_register: str, callbacks: dict) -> None:
+def test_b5dc_set_attenuation_with_valid_input(
+    attenuation_register: str, callbacks: dict, b5dc_cm_setup
+) -> None:
     """Verify SetAttenuation returns expected LRC updates on supplying valid input."""
-    update_period = 2
-    with patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcInterface", AsyncMock()), patch(
-        "ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcPropertyParser", Mock()
-    ), patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcProtocol", Mock()):
-        b5dc_cm = B5dcDeviceComponentManager("127.0.0.1", 10001, update_period, Mock())
-        b5dc_cm.start_communicating()
-
-    max_try = 5
-    for iterations in range(max_try):
-        if not b5dc_cm.is_connection_established():
-            time.sleep(1)
-        else:
-            break
-
-    assert iterations < max_try - 1, "Connection not established"
-
+    b5dc_cm, _ = b5dc_cm_setup
     b5dc_cm._b5dc_device_attn_conf = AsyncMock()
 
     # Set the attenuation and verify that expected lrc updates are published
@@ -186,24 +140,10 @@ def test_b5dc_set_attenuation_with_valid_input(attenuation_register: str, callba
     ],
 )
 def test_b5dc_set_attenuation_with_invalid_input(
-    attenuation_register: str, callbacks: dict
+    attenuation_register: str, callbacks: dict, b5dc_cm_setup
 ) -> None:
     """Verify SetAttenuation returns expected LRC updates on invalid input."""
-    update_period = 2
-    with patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcInterface", AsyncMock()), patch(
-        "ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcPropertyParser", Mock()
-    ), patch("ska_mid_dish_b5dc_proxy.b5dc_cm.B5dcProtocol", Mock()):
-        b5dc_cm = B5dcDeviceComponentManager("127.0.0.1", 10001, update_period, Mock())
-        b5dc_cm.start_communicating()
-
-    max_try = 5
-    for iterations in range(max_try):
-        if not b5dc_cm.is_connection_established():
-            time.sleep(1)
-        else:
-            break
-
-    assert iterations < max_try - 1, "Connection not established"
+    b5dc_cm, _ = b5dc_cm_setup
 
     b5dc_cm._b5dc_device_attn_conf.set_attenuation = AsyncMock(
         side_effect=B5dcDeviceAttenuationException("ex")
